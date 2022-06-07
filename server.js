3fc45217f550c1da41d023951e3e8c12fe84e0af
Arreola <ra3020343@gmail.com>
2022-06-06 22:27:02 -0700

    workout api completed

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require ('express')
+const app = express()
+const PORT = 8000
+
+
+const workouts = {
+        'biceps':{
+            'reps': 4,
+            'exercise': 'bicep curls',
+            
+    },
+        'legs':{
+            'reps': 4,
+            'exercise': 'squats',
+    },
+        'unknown':{
+            'reps': 0,
+            'exercise': 'unknown',
+
+        }
+
+
+
+}
+
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+
+})
+
+app.get('/api/:workoutName', (request, response)=>{
+    const workoutNames = request.params.workoutName.toLowerCase()
+    if(workouts [workoutNames]){
+        response.json(workouts[workoutNames])
+    }else{
+        response.json(workouts['unknown'])
+    }   
+    
+})
+
+app.listen(PORT,()=>{
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+} )
